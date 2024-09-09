03d06fdeddf4cf75de4722953f5fa83fb1943359
patoneill24 <poneill33@byui.edu>
2024-09-09 11:11:03 -0600

    javascript for view more buttons

--- scripts/backToTop.js
+++ scripts/backToTop.js
@@ -19,3 +19,5 @@ backToTopBtn.addEventListener('click', function() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 });
+
+
