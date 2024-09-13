f989e7faf14c07aeffcc2f8741e368c81e8bdd57
patoneill24 <poneill33@byui.edu>
2024-09-13 15:20:58 -0600

    top rated/best priced

--- scripts/bestPrice.js
+++ scripts/bestPrice.js
@@ -13,39 +13,6 @@ async function initialize(){
 
         let maleHousing = document.getElementById("best-price-male");
         let femaleHousing = document.getElementById("best-price-female");
-
-        function obj_sort_asc(a,b){
-            if (a.priceMin < b.priceMin) {
-                return -1;
-            } else if (a.priceMin > b.priceMin){
-                return 1;
-            } else{
-                return 0;
-            }
-        }
-
-        function bestPrice(data, location){
-            // let bestPrice = data[0].priceMin;
-            // let bestApartment = data[0];
-            // data.forEach(item => {
-            //     if(item.priceMin < bestPrice){
-            //         bestPrice = item.priceMin;
-            //         bestApartment = item;
-            //     }
-            // });
-            data.sort(obj_sort_asc);
-            bestApartments=data.slice(0,6);
-            bestApartments.forEach(bestApartment => {
-                location.innerHTML += `<a href="#"><div class="productbox">
-                <img class="homeHousingImage" src="${bestApartment.imageUrl}" alt=${bestApartment.name}>
-                <h2>${bestApartment.name}</h2>
-                <h4>$${bestApartment.priceMin}<h4>
-                <h3>${bestApartment.rating} / 5</h3>
-                </div></a>`;
-            });
-        }
-        bestPrice(maleApartments, maleHousing);
-        bestPrice(femaleApartments, femaleHousing);
     }catch(error){
         console.log(error);
     }
--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ async function initialize(){
         const femaleApartments = await fetchJson('data/femaleApartments.json');
         function output(data, location){
             data.forEach(item => {
-                if(item.priceMin === item.price){
+                if(item.priceMin === item.priceMax){
                     location.innerHTML += `<a href="#"><div class="productbox">
                     <img class="homeHousingImage" src="${item.imageUrl}" alt=${item.name}>
                     <h2>${item.name}</h2>
@@ -95,6 +95,7 @@ async function initialize(){
                 //     }
                 // });
             }
+            window.close();
         });
 
         document.querySelector("#viewMoreMale").addEventListener("click", function(){
@@ -121,6 +122,100 @@ async function initialize(){
                 //     }
                 // });
             }
+            window.close();
+        });
+
+        function obj_sort_asc_price(a,b){
+            if (a.priceMin < b.priceMin) {
+                return -1;
+            } else if (a.PriceMin > b.PriceMax){
+                return 1;
+            } else{
+                return 0;
+            }
+        }
+
+        function obj_sort_dec_rating(a,b){
+            if (a.rating > b.rating) {
+                return -1;
+            } else if (a.rating < b.rating){
+                return 1;
+            } else{
+                return 0;
+            }
+        }
+
+
+        function bestPrice(data, location){
+            // let bestPrice = data[0].priceMin;
+            // let bestApartment = data[0];
+            // data.forEach(item => {
+            //     if(item.priceMin < bestPrice){
+            //         bestPrice = item.priceMin;
+            //         bestApartment = item;
+            //     }
+            // });
+            data.sort(obj_sort_asc_price);
+            bestApartments=data.slice(0,6);
+            // bestApartments.forEach(bestApartment => {
+            //     location.innerHTML += `<a href="#"><div class="productbox">
+            //     <img class="homeHousingImage" src="${bestApartment.imageUrl}" alt=${bestApartment.name}>
+            //     <h2>${bestApartment.name}</h2>
+            //     <h4>$${bestApartment.priceMin}<h4>
+            //     <h3>${bestApartment.rating} / 5</h3>
+            //     </div></a>`;
+            // });
+            output(bestApartments, location);
+        }
+
+        function bestRating(data, location){
+            data.sort(obj_sort_dec_rating);
+            bestApartments=data.slice(0,6);
+            output(bestApartments, location);
+        }
+
+        document.querySelector("#femaleBestPrice").addEventListener("click", function(){
+            var newWindow = window.open("bestApt.html");
+            newWindow.onload = function(){
+                let title = newWindow.document.querySelector("#apartments-title");
+                let femaleHousing = newWindow.document.querySelector("#best-apartments");
+                title.innerHTML = "Best Priced Female Apartments";
+                bestPrice(femaleApartments, femaleHousing);
+            }
+            window.close();
+        });
+        
+        document.querySelector("#maleBestPrice").addEventListener("click", function(){
+            var newWindow = window.open("bestApt.html");
+            newWindow.onload = function(){
+                let title = newWindow.document.querySelector("#apartments-title");
+                let maleHousing = newWindow.document.querySelector("#best-apartments");
+                title.innerHTML = "Best Priced Male Apartments";
+                bestPrice(maleApartments, maleHousing);
+            }
+            window.close();
+        });
+
+        document.querySelector("#femaleBestRating").addEventListener("click", function(){
+            var newWindow = window.open("bestApt.html");
+            newWindow.onload = function(){
+                let title = newWindow.document.querySelector("#apartments-title");
+                let femaleHousing = newWindow.document.querySelector("#best-apartments");
+                title.innerHTML = "Best Rated Female Apartments";
+                bestRating(femaleApartments, femaleHousing);
+            }
+            window.close();
+        });
+
+        document.querySelector("#maleBestRating").addEventListener("click", function(){
+            var newWindow = window.open("bestApt.html");
+            newWindow.onload = function(){
+                let title = newWindow.document.querySelector("#apartments-title");
+                let maleHousing = newWindow.document.querySelector("#best-apartments");
+                title.innerHTML = "Best Rated Male Apartments";
+                bestRating(maleApartments, maleHousing);
+            }
+            window.close();
         });
     }catch(error){
         console.error(error);
